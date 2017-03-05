5a2561d18d47cc1eb5ce5c0b588c612b2a95fda5
Alain O'Dea <alain.odea@gmail.com>
2017-03-05 13:08:21 -0330

    Make executable pipeline friendly

--- app/Main.hs
+++ app/Main.hs
@@ -18,55 +18,93 @@ import Network.Tenable.SecurityCenter.Token
 import Network.Tenable.SecurityCenter.Types
 import Network.Tenable.SecurityCenter.Asset
 
-import           Control.Monad (liftM)
 import           Data.Aeson
+import           Data.Aeson.Types
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.Text.IO as T
 import qualified Data.Text as T
 import           Network.HTTP.Conduit
+import           System.Environment (getArgs)
 import           System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
+import           Data.Either (either)
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  putStr "Tenable SecurityCenter Host: "
-  host <- S8.getLine
-  putStr "Username: "
-  u <- T.getLine
-  putStr "Password: "
-  p <- T.getLine
-  (t, session) <- getToken host u p
-  let assetToUpdate = "186"
-  a <- getAssetById session host t assetToUpdate
-  print a
-  let Just definedIPs = (fmap (
-                       (T.splitOn ",").
-                       typeFieldsDefinedIPs.
-                       assetByIdStaticTypeFields
-                       ) $
-                    (a :: Maybe GetAssetByIdResponse))
-  print definedIPs
-  updateDefinedIPs session host t assetToUpdate definedIPs
+  [configFilename, assetIdArg] <- getArgs
+  configFile <- L8.readFile configFilename
+  let config = either error id $ eitherDecode configFile
+  let hostname = textToS8 . securityCenterHost $ config
+  let u = securityCenterUsername config
+  let p = securityCenterPassword config
+  rawUpdate <- T.getContents
+  let definedIPs = T.lines rawUpdate
+  let assetToUpdate = S8.pack assetIdArg
+  (t, session) <- getToken hostname u p
+  _ <- updateDefinedIPs session hostname t assetToUpdate definedIPs
+  _ <- endSession hostname (session, t)
+  T.putStrLn "All done!"
+
+data Config = Config
+              { securityCenterHost :: T.Text
+              , securityCenterUsername :: T.Text
+              , securityCenterPassword :: T.Text
+              }
+
+instance FromJSON Config where
+  parseJSON (Object v) = Config <$>
+                         v .: "host" <*>
+                         v .: "username" <*>
+                         v .: "password"
+  parseJSON invalid = typeMismatch "Config" invalid
+
+textToS8 :: T.Text
+         -> S8.ByteString
+textToS8 = S8.pack . T.unpack
 
 getToken :: S8.ByteString
          -> T.Text
          -> T.Text
          -> IO (Int, CookieJar)
-getToken host u p = do
-  let session = createCookieJar []
+getToken hostname u p = do
+  let unauthSession = createCookieJar []
   let req = CreateTokenRequest { username = u, password = p }
-  (res, session) <- runRequest host req session
+  (res, authSession) <- runRequest hostname req unauthSession
   let Just t = fmap (token.response) res
-  return (t, session)
+  return (t, authSession)
+
+updateDefinedIPs :: CookieJar
+                 -> S8.ByteString
+                 -> Int
+                 -> S8.ByteString
+                 -> [T.Text]
+                 -> IO (Maybe GetAssetByIdResponse)
+updateDefinedIPs session hostname t assetToUpdate definedIPs = do
+  let req = UpdateDefinedIPsRequest
+            { updateDefinedIPsAssetId = assetToUpdate
+            , updateDefinedIPsToken = (S8.pack . show) t
+            , updateDefinedIPsDefinedIPs = definedIPs
+            }
+  (res, _) <- runRequest hostname req session
+  return $ fmap response res
+
+endSession :: S8.ByteString
+           -> (CookieJar, Int)
+           -> IO (Maybe (ApiResponse Object), CookieJar)
+endSession hostname (session, t) = do
+  let req = DeleteTokenRequest
+            { deleteTokenRequestToken = (S8.pack . show) t
+            }
+  runRequest hostname req session
 
 listAssets :: CookieJar
            -> S8.ByteString
            -> Int
            -> IO ()
-listAssets session host t = do
+listAssets session hostname t = do
    let req = ListAssetsRequest { authenticationToken = (S8.pack . show) t }
-   (res, session) <- runRequest host req session
+   (res, _) <- runRequest hostname req session
    let usables = fmap (usableAssets.response) res
    print usables
    let manageables = fmap (manageableAssets.response) res
@@ -77,26 +115,10 @@ getAssetById :: CookieJar
              -> Int
              -> S8.ByteString
              -> IO (Maybe GetAssetByIdResponse)
-getAssetById session host t assetToUpdate = do
+getAssetById session hostname t assetToUpdate = do
   let req = GetAssetByIdRequest
              { getAssetByIdToken = (S8.pack . show) t
              , getAssetByIdId = assetToUpdate
              }
-  (res, session) <- runRequest host req session
+  (res, _) <- runRequest hostname req session
   return $ fmap response res
-
-updateDefinedIPs :: CookieJar
-                 -> S8.ByteString
-                 -> Int
-                 -> S8.ByteString
-                 -> [T.Text]
-                 -> IO ()
-updateDefinedIPs session host t assetToUpdate definedIPs = do
-  let req = UpdateDefinedIPsRequest
-            { updateDefinedIPsAssetId = assetToUpdate
-            , updateDefinedIPsToken = (S8.pack . show) t
-            , updateDefinedIPsDefinedIPs = definedIPs
-            }
-  (res, session) <- runRequest host req session
-  let assetRes = fmap response res
-  print (assetRes :: Maybe GetAssetByIdResponse)
